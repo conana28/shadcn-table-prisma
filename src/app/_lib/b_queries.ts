5cf9b08358586732746f8979a2d6dbcb3706355a
conana28 <33334673+conana28@users.noreply.github.com>
2024-02-13 10:11:44 +1300

    init with new / _components & /_lib

---
+++ src/app/_lib/b_queries.ts
@@ -0,0 +1,100 @@
+"use server"
+
+import { SearchParams } from "@/types"
+import { Priority, Prisma, Status } from "@prisma/client"
+
+import { filterColumn } from "@/lib/filter-column"
+import prisma from "@/lib/prisma"
+import { searchParamsSchema } from "@/lib/validations/params"
+
+type Task = {
+  id: string
+  code: string
+  title: string
+  status: "todo" | "in_progress" | "done" | "canceled"
+  label: "bug" | "feature" | "enhancement" | "documentation"
+  priority: "low" | "medium" | "high"
+}
+
+type tasksWhereInput = Prisma.tasksWhereInput
+
+export async function getTasks(searchParams: SearchParams) {
+  try {
+    const { page, per_page, sort, title, code, status, priority, operator } =
+      searchParamsSchema.parse(searchParams)
+
+    console.log("sort:", sort)
+    const pageAsNumber = Number(page)
+    const fallbackPage =
+      isNaN(pageAsNumber) || pageAsNumber < 1 ? 1 : pageAsNumber
+    const perPageAsNumber = Number(per_page)
+    const limit = isNaN(perPageAsNumber) ? 10 : perPageAsNumber
+    const offset = fallbackPage > 0 ? (fallbackPage - 1) * limit : 0
+    const [column, order] = (sort?.split(".") as [
+      keyof Task | undefined,
+      "asc" | "desc" | undefined,
+    ]) ?? ["title", "desc"]
+
+    const statuses = (status?.split(".") as Task["status"][]) ?? []
+    const priorities = (priority?.split(".") as Task["priority"][]) ?? []
+
+    function buildWhereClause({
+      code,
+      title,
+      statuses,
+      priorities,
+    }: {
+      title?: string
+      code?: string
+      statuses?: string[]
+      priorities?: string[]
+    }): tasksWhereInput | undefined {
+      const conditions: Array<tasksWhereInput> = []
+
+      if (title) {
+        conditions.push({ title: { contains: title } })
+      }
+      if (code) {
+        conditions.push({ code: { contains: code } })
+      }
+      if (statuses?.length ?? 0 > 0) {
+        const statusArray: Status[] = statuses as Status[] // Add type assertion here
+        conditions.push({ status: { in: statusArray } })
+      }
+
+      if (priorities?.length ?? 0 > 0) {
+        const prioritesArray: Priority[] = priorities as Priority[] // Declare prioritesArray variable
+        conditions.push({ priority: { in: prioritesArray } })
+      }
+      console.log(conditions)
+      // Combine conditions with OR if any are present
+      return conditions.length > 0 ? { OR: conditions } : undefined
+    }
+
+    const whereClause = buildWhereClause({
+      code: code,
+      title: title,
+      statuses: statuses,
+      priorities: priorities,
+    })
+
+    const data = await prisma.tasks.findMany({
+      where: whereClause,
+      orderBy: {
+        [column ?? "id"]: order ?? "desc",
+      },
+      skip: offset,
+      take: limit,
+    })
+
+    const count = await prisma.tasks.count({
+      where: whereClause,
+    })
+
+    const pageCount = Math.ceil(count / limit)
+    return { data, pageCount }
+  } catch (err) {
+    console.log(err)
+    return { data: [], pageCount: 0 }
+  }
+}
